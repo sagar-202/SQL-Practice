7a44afeae3f46f5d6f279157288414c99e313dbc
sagar-202 <sagarpatgar@gmail.com>
2026-07-05 11:24:53 +0530

    Time: 472 ms (75.56%) | Memory: 0B (100.00%) - LeetSync

---
+++ 197-rising-temperature/rising-temperature.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select today.id
+from Weather yesterday
+cross join Weather today
+
+where datediff(today.recordDate,yesterday.recordDate) = 1
+And today.temperature > yesterday.temperature;
